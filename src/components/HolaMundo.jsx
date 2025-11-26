2c0d82b30ce63c714111d9022d03bcbaa17782b9
Feliverse <haybar@gmail.com>
2025-11-26 19:40:05 -0400

    Actulizacion de HolaMundo

--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import './HolaMundo.css';
 
 const HolaMundo = () => {
     return (
         <div>
             <h1>¡Hola, Mundo!</h1>
             <p>Bienvenido a mi aplicación React.</p>
-            <p>Esta aplicacion esta en produccion</p>
-            <p>PROBANDO VELOCIDAD DE CAMBIO EN PRODUCCION</p>
         </div>
     );
 };
